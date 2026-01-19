452b742973156ca7d330b4a307c7b9e544a935b4
Mohid Dawod <mohidawod@gmail.com>
2026-01-19 14:58:35 +0200

    fix: update compileSdk and targetSdk to 36 for camera plugin compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.sudan.passport.photo"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -22,7 +22,7 @@ android {
     defaultConfig {
         applicationId = "com.sudan.passport.photo"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
